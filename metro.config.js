114a8b04265da1d95966c12aff91b5537e32546c
GScriptS <simoneguarnuccio@gmail.com>
2023-09-12 22:42:23 +0200

    feat: update metro config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,7 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.resolverMainFields = [
-  "sbmodern",
-  ...defaultConfig.resolver.resolverMainFields,
-];
-
-module.exports = defaultConfig;
+module.exports = config;
